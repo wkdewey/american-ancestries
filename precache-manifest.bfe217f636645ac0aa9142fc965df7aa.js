2573a57ad621ec23a64ae701c6afbcf35f945cdc
wkdewey <wkdewey@gmail.com>
2021-02-08 19:00:03 -0500

    Updates

---
+++ precache-manifest.bfe217f636645ac0aa9142fc965df7aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613d1129bdae2890b4eb2be0e4d17922",
+    "url": "/american-ancestries/index.html"
+  },
+  {
+    "revision": "23fed4730eea4b3829b5",
+    "url": "/american-ancestries/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "6326e07c7e5fbe885607",
+    "url": "/american-ancestries/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "23fed4730eea4b3829b5",
+    "url": "/american-ancestries/static/js/2.369b1a52.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/american-ancestries/static/js/2.369b1a52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6326e07c7e5fbe885607",
+    "url": "/american-ancestries/static/js/main.815d895d.chunk.js"
+  },
+  {
+    "revision": "cf1775a8ecc2ac1f4d7e",
+    "url": "/american-ancestries/static/js/runtime-main.672cb0b4.js"
+  }
+]);
