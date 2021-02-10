63415a542012e32f2928e8081f7b17954c00073c
wkdewey <wkdewey@gmail.com>
2021-02-10 15:26:11 -0500

    Updates

---
+++ precache-manifest.1cb518b0359ae529d34750ab26249fc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0dc70e801c3cfe8bf3745ebcfff89aa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "23fed4730eea4b3829b5",
+    "url": "./static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "a86810c4b8b766a7197e",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "23fed4730eea4b3829b5",
+    "url": "./static/js/2.369b1a52.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.369b1a52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a86810c4b8b766a7197e",
+    "url": "./static/js/main.b2c5d7ba.chunk.js"
+  },
+  {
+    "revision": "f989863515067ac11d69",
+    "url": "./static/js/runtime-main.2cd3126c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/american-ancestries/precache-manifest.56358cc4d43751673af7e8cd870188ef.js"
+  "./precache-manifest.1cb518b0359ae529d34750ab26249fc1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/american-ancestries/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
