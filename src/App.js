d16e5d6f4666452eaf0ef65705a641f4115c4d77
wkdewey <wkdewey@gmail.com>
2021-05-17 14:12:16 -0500

    import components and actions, connect to Redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+import { fetchPlaces } from "./actions/placeActions";
+import { fetchAncestryGroups } from "./actions/ancestryGroupActions";
 import "./App.css";
 import NavBar from "./components/NavBar";
-import PlacesContainer from "./containers/PlacesContainer";
-import AncestryGroupsContainer from "./containers/AncestryGroupsContainer";
+// import PlacesContainer from "./containers/PlacesContainer";
+// import AncestryGroupsContainer from "./containers/AncestryGroupsContainer";
+import AncestryGroups from "./components/ancestry_groups/AncestryGroups";
+import AncestryGroup from "./components/ancestry_groups/AncestryGroup";
+import Place from "./components/places/Place";
+import PlaceComparison from "./components/places/PlaceComparison";
+import PlaceInput from "./components/places/PlaceInput";
+import Places from "./components/places/Places";
 import Home from "./components/Home";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchPlaces();
+    this.props.fetchAncestryGroups();
+  }
   render() {
+    const ancestryGroups = this.props.ancestryGroups;
+    const places = this.props.places;
+    const placeAncestryGroups = this.props.placeAncestryGroups;
     return (
       <Router>
         <div className="App">
@@ -53,5 +69,19 @@ class App extends Component {
     );
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    ancestryGroups: state.ancestryGroups,
+    places: state.places,
+    placeAncestryGroups: state.placeAncestryGroups,
+  };
+};
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchPlaces: () => dispatch(fetchPlaces()),
+    fetchAncestryGroups: () => dispatch(fetchAncestryGroups()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
